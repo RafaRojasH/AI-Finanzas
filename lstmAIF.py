fca46ba7bdaa0b533fe9dcb04ca44bf4c0e55da5
RafaRojasH <94881844+RafaRojasH@users.noreply.github.com>
2021-12-07 16:09:41 -0600

    Update lstmAIF.py

--- lstmAIF.py
+++ lstmAIF.py
@@ -65,9 +65,9 @@ def LongShortTM(df):
 
     # calculate root mean squared error
     trainScore = math.sqrt(mean_squared_error(trainY[0], trainPredict[:, 0]))
-    print('Train Score: %.2f RMSE' % (trainScore))
+    st.write('Train Score: %.2f RMSE' % (trainScore))
     testScore = math.sqrt(mean_squared_error(testY[0], testPredict[:, 0]))
-    print('Test Score: %.2f RMSE' % (testScore))
+    st.write('Test Score: %.2f RMSE' % (testScore))
     # shift train predictions for plotting
     trainPredictPlot = np.empty_like(dataset)
     trainPredictPlot[:, :] = np.nan
@@ -77,7 +77,6 @@ def LongShortTM(df):
     testPredictPlot[:, :] = np.nan
     testPredictPlot[len(trainPredict) + (look_back * 2) + 1:len(dataset) - 1, :] = testPredict
     # plot baseline and predictions
-    plt.plot(scaler.inverse_transform(dataset))
-    plt.plot(trainPredictPlot)
-    plt.plot(testPredictPlot)
-    plt.show()
+    st.line_chart(scaler.inverse_transform(dataset))
+    st.line_chart(trainPredictPlot)
+    st.line_chart(testPredictPlot)
