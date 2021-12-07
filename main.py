cc640cb619113864a767d08260de82d67d979cf9
RafaRojasH <94881844+RafaRojasH@users.noreply.github.com>
2021-12-07 16:11:11 -0600

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from tensorflow.keras.layers import LSTM
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_squared_error
 from tensorflow.keras.backend import clear_session
+from lstmAIF import LongShortTM
 
 st.title('AI & Finanzas')
 st.subheader('Consultar Stock en un intervalo de tiempo')
@@ -74,44 +75,8 @@ for i in df_indices_principales.index:
                         modelos = ['LSTM', 'opc1', 'opc2', 'opc3']
                         modelo = st.sidebar.selectbox('Elige un modelo', modelos)
                         if modelo == 'LSTM':
-                            def create_dataset(dataset, look_back=1):
-                                dataX, dataY = [], []
-                                for i in range(len(dataset) - look_back - 1):
-                                    a = dataset[i:(i + look_back), 0]
-                                    dataX.append(a)
-                                    dataY.append(dataset[i + look_back, 0])
-                                return np.array(dataX), np.array(dataY)
-                            dataset = df.iloc[:, 1:2].values
-                            dataset = dataset.astype('float32')
-                            porcent_train = 75
-                            scaler = MinMaxScaler(feature_range=(0, 1))
-                            dataset = scaler.fit_transform(dataset)
-                            train_size = int(len(dataset) * (porcent_train / 100))
-                            test_size = len(dataset) - train_size
-                            train, test = dataset[0:train_size, :], dataset[train_size:len(dataset), :]
-                            # reshape into X=t and Y=t+1
-                            look_back = 1
-                            trainX, trainY = create_dataset(train, look_back)
-                            testX, testY = create_dataset(test, look_back)
-                            # reshape input to be [samples, time steps, features]
-                            trainX = np.reshape(trainX, (trainX.shape[0], 1, trainX.shape[1]))
-                            testX = np.reshape(testX, (testX.shape[0], 1, testX.shape[1]))
-                            # create and fit the LSTM network
-                            model = Sequential()
-                            model.add(LSTM(4, input_shape=(trainX.shape[1], look_back)))
-                            model.add(Dense(1))
-                            model.compile(loss='mean_squared_error', optimizer='adam')
-
-                            model.fit(trainX, trainY, epochs=100, batch_size=1, verbose=2)
-                            # make predictions
-                            trainPredict = model.predict(trainX)
-                            testPredict = model.predict(testX)
-                            # invert predictions
-                            trainPredict = scaler.inverse_transform(trainPredict)
-                            trainY = scaler.inverse_transform([trainY])
-                            testPredict = scaler.inverse_transform(testPredict)
-                            testY = scaler.inverse_transform([testY])
-                            st.sidebar.write('FIN')
+                            LongShortTM(df)
+                            
 
                     except ValueError:
                         exc = format_exc()
