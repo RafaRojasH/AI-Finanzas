be22954da1999a69f08d4e4d9fc55dca781d84d1
RafaRojasH <94881844+RafaRojasH@users.noreply.github.com>
2021-12-07 08:37:28 -0600

    Update main.py

--- main.py
+++ main.py
@@ -81,7 +81,36 @@ for i in df_indices_principales.index:
                                     dataY.append(dataset[i + look_back, 0])
                                 return np.array(dataX), np.array(dataY)
                             dataset = df.iloc[:, 1:2].values
-                        
+                            dataset = dataset.astype('float32')
+                            porcent_train = 75
+                            scaler = MinMaxScaler(feature_range=(0, 1))
+                            dataset = scaler.fit_transform(dataset)
+                            train_size = int(len(dataset) * (porcent_train / 100))
+                            test_size = len(dataset) - train_size
+                            train, test = dataset[0:train_size, :], dataset[train_size:len(dataset), :]
+                            # reshape into X=t and Y=t+1
+                            look_back = 1
+                            trainX, trainY = create_dataset(train, look_back)
+                            testX, testY = create_dataset(test, look_back)
+                            # reshape input to be [samples, time steps, features]
+                            trainX = numpy.reshape(trainX, (trainX.shape[0], 1, trainX.shape[1]))
+                            testX = numpy.reshape(testX, (testX.shape[0], 1, testX.shape[1]))
+                            # create and fit the LSTM network
+                            model = Sequential()
+                            model.add(LSTM(4, input_shape=(1, look_back)))
+                            model.add(Dense(1))
+                            model.compile(loss='mean_squared_error', optimizer='adam')
+                            model.fit(trainX, trainY, epochs=100, batch_size=1, verbose=2)
+                            # make predictions
+                            trainPredict = model.predict(trainX)
+                            testPredict = model.predict(testX)
+                            # invert predictions
+                            trainPredict = scaler.inverse_transform(trainPredict)
+                            trainY = scaler.inverse_transform([trainY])
+                            testPredict = scaler.inverse_transform(testPredict)
+                            testY = scaler.inverse_transform([testY])
+                            st.sidebar.write('FIN')
+
                     except ValueError:
                         exc = format_exc()
                         st.text_input('Error', exc)
@@ -124,9 +153,6 @@ for i in df_indices_principales.index:
                                        data=csv,
                                        file_name=nombre_consulta,
                                        mime='text/csv')
-                
-
-
             else:
                 st.sidebar.text_input('Error', 'El índice no tiene stocks')
 
