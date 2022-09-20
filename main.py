0807b555b8c22a398c2a8074d4df8cc508407cda
RafaRojasH <94881844+RafaRojasH@users.noreply.github.com>
2022-09-20 13:57:29 -0500

    Update main.py

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ for i in df_indices_principales.index:
 
                 if st.sidebar.button('Consultar'):
                     try:
-                        consulta = investpy.get_stock_historical_data(stock=600010,
+                        consulta = investpy.get_stock_historical_data(stock=stock_symbol,
                                                                       country='china',
                                                                       from_date='01/01/2000',
                                                                       to_date=str('01/09/2022'))
